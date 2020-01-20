b67c3afa0dc6e041b56970d2c7c460483d78ebc9
dskrypa <dskrypa@gmail.com>
2020-01-20 14:22:14 -0500

    added additional test; updated version + setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-from setuptools import setup, find_packages
+from setuptools import setup
 
 project_root = Path(__file__).resolve().parent
 
@@ -10,16 +10,22 @@ with project_root.joinpath('requirements.txt').open('r', encoding='utf-8') as f:
 with project_root.joinpath('readme.rst').open('r', encoding='utf-8') as f:
     long_description = f.read()
 
+about = {}
+with project_root.joinpath('requests_client', '__version__.py').open('r', encoding='utf-8') as f:
+    exec(f.read(), about)
+
 
 setup(
-    name='requests_client',
-    version='2020.01.19',
-    author='Doug Skrypa',
-    author_email='dskrypa@gmail.com',
-    description='Requests Client',
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    description=about['__description__'],
     long_description=long_description,
-    url='https://github.com/dskrypa/requests_client',
-    packages=find_packages(),
+    url=about['__url__'],
+    project_urls={'Source': 'https://github.com/dskrypa/requests_client'},
+    packages=['requests_client'],
+    license=about['__license__'],
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
@@ -30,5 +36,6 @@ setup(
         'Programming Language :: Python :: 3.8'
     ],
     python_requires='~=3.4',
-    install_requires=['wheel'] + requirements
+    install_requires=['requests'],
+    tests_require=['flask']
 )
---
+++ tests/test_requests.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+"""
+Test the RequestsClient with actual HTTP requests.
+
+:author: Doug Skrypa
+"""
+
+import logging
+import os
+import socket
+import sys
+import unittest
+from concurrent.futures import as_completed, ThreadPoolExecutor
+from pathlib import Path
+from threading import Thread
+
+from flask import Flask, request
+
+sys.path.append(Path(__file__).parents[1].as_posix())
+from requests_client import RequestsClient
+
+log = logging.getLogger(__name__)
+
+
+def find_free_port():
+    s = socket.socket()
+    s.bind(('', 0))
+    return s.getsockname()[1]
+
+
+class RequestsClientFlaskTest(unittest.TestCase):
+    def test_requests(self):
+        print()
+        response = 'Test app response'
+        app = Flask(__name__)
+        os.environ['FLASK_ENV'] = 'dev'
+
+        @app.route('/')
+        def root():
+            return response
+
+        @app.route('/shutdown')
+        def shutdown_server():
+            request.environ.get('werkzeug.server.shutdown')()
+            return ''   # Prevents: TypeError: The view function did not return a valid response...
+
+        port = find_free_port()
+        app_thread = Thread(target=app.run, args=('localhost', port))
+        app_thread.start()
+
+        client = RequestsClient('localhost', port)
+        threads = 5
+        with ThreadPoolExecutor(max_workers=threads) as exectuor:
+            _futures = [exectuor.submit(client.get, '/') for _ in range(threads)]
+            for future in as_completed(_futures):
+                resp = future.result()
+                log.debug('Result: {}'.format(resp))
+                self.assertEqual(resp.text, response)
+
+        client.get('/shutdown', raise_errors=False)
+        app_thread.join()
+
+
+if __name__ == '__main__':
+    try:
+        unittest.main(warnings='ignore', verbosity=2, exit=False)
+    except KeyboardInterrupt:
+        print()
