973d88de4a68b704e75695c701a9cd155773ce67
dskrypa <dskrypa@gmail.com>
2020-09-19 15:46:14 -0400

    ran pre-commit autoupdate

--- requests_client/__version__.py
+++ requests_client/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'requests_client'
 __description__ = 'Requests Client'
 __url__ = 'https://github.com/dskrypa/requests_client'
-__version__ = '2020.09.19-9'
+__version__ = '2020.09.19-10'
 __author__ = 'Doug Skrypa'
 __author_email__ = 'dskrypa@gmail.com'
 __license__ = 'Apache 2.0'
